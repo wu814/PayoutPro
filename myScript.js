79b76f8c7fe22196116b5b8901d7d6793f3f0a28
wu814 <william14.wu@gmail.com>
2023-01-29 03:48:00 -0600

    New background

--- myScript.js
+++ myScript.js
@@ -1,6 +1,3 @@
-function test(){
-    var test = document.getElementById('test');
-    var name1 = document.getElementById('player1name');
-    
-    test.innerHTML = "hi " + name1.value;
+function calculate(){
+    console.log("hello")
 }
